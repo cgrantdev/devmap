822b65f120d0fd6ed0f2a507bee0f14d518ece9d
Your Name <you@example.com>
2025-06-24 07:13:00 +0200

    remove unnecessary

--- resources/js/Pages/Auth/VerifyEmail.vue
+++ resources/js/Pages/Auth/VerifyEmail.vue
@@ -104,13 +104,6 @@
             >
               Go to Dashboard
             </Link>
-            
-            <Link
-              href="/login"
-              class="w-full flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
-            >
-              Back to Login
-            </Link>
           </div>
         </div>
       </div>
